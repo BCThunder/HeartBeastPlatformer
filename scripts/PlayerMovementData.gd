4d92b9d16f7977d9c720c0c2d08692e2e0a70d30
BCSchool <BCSchool3000@gmail.com>
2024-06-14 12:07:18 -0700

    Added to Player Scripts
    -Created Player movement data resource that stores all player movements constants, such as speed and acceleration.
    
    -Added Double Jump and Wall Jump

---
+++ scripts/PlayerMovementData.gd
@@ -0,0 +1,9 @@
+class_name PlayerMovementData
+extends Resource
+
+@export var speed = 150.0
+@export var jump_velocity = -300.0
+@export var acceleration = 400.0
+@export var friction = 5000.0
+@export var air_resistance = 2500.0
+@export var air_acceleration = 200.0
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,26 +1,34 @@
 extends CharacterBody2D
 
 
-const SPEED = 150
-const JUMP_VELOCITY = -300.0
-const ACCELERATION = 400
-const FRICTION = 5000
+@export var movement_data : PlayerMovementData
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+var air_jump = false
+var just_wall_jumped = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var animated_sprite_2d = $AnimatedSprite2D
 
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var coyote_jump_timer = $CoyoteJumpTimer
 
 func _physics_process(delta):
 	apply_gravity(delta)
+	handle_wall_jump()
 	handle_jump()
 	
 	var direction = Input.get_axis("MoveLeft", "MoveRight")
 	handle_acceleration(direction, delta)
-	handle_friction(direction, delta)
+	handle_air_acceleration(direction, delta)
+	apply_friction(direction, delta)
+	apply_air_resistance(direction, delta)
 	update_animations(direction)
-
+		
 	move_and_slide()
+	
+	var was_on_floor = is_on_floor()
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	if just_left_ledge:
+		coyote_jump_timer.start()
+	just_wall_jumped = false
 	
 func apply_gravity(delta):
 	# Add the gravity.
@@ -28,24 +36,52 @@ func apply_gravity(delta):
 		velocity.y += gravity * delta
 
 
+func handle_wall_jump():
+	if not is_on_wall_only(): return
+	
+	var wall_normal = get_wall_normal()
+	if (Input.is_action_just_pressed("MoveLeft") and wall_normal == Vector2.LEFT) or \
+	(Input.is_action_just_pressed("MoveRight") and wall_normal == Vector2.RIGHT):
+		velocity.x = wall_normal.x * movement_data.speed
+		velocity.y = movement_data.jump_velocity
+		just_wall_jumped = true
+
+
 func handle_jump():
-	# Handle jump.
 	if is_on_floor():
+		air_jump = true
+
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("Jump"):
-			velocity.y = JUMP_VELOCITY
-	else:
-		if Input.is_action_just_pressed("Jump") and velocity.y < JUMP_VELOCITY / 2:
-			velocity.y = JUMP_VELOCITY / 2
+			velocity.y = movement_data.jump_velocity
+	if not is_on_floor():
+		if Input.is_action_just_pressed("Jump") and velocity.y < movement_data.jump_velocity / 2:
+			velocity.y = movement_data.jump_velocity / 2
+			
+		if Input.is_action_just_pressed("Jump") and air_jump and not just_wall_jumped:
+			velocity.y = movement_data.jump_velocity * 0.8
+			air_jump = false
 
 
 func handle_acceleration(direction, delta):
+	if not is_on_floor(): return
 	if direction != 0:
-		velocity.x = move_toward(velocity.x, SPEED * direction, ACCELERATION * delta)
+		velocity.x = move_toward(velocity.x, movement_data.speed * direction, movement_data.acceleration * delta)
 
 
-func handle_friction(direction, delta):
-	if direction == 0:
-		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+func handle_air_acceleration(direction, delta):
+	if is_on_floor(): return
+	if direction != 0:
+		velocity.x = move_toward(velocity.x, movement_data.speed * direction, movement_data.air_acceleration * delta)
+
+func apply_friction(direction, delta):
+	if direction == 0 and is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, movement_data.friction * delta)
+
+
+func apply_air_resistance(direction, delta):
+	if direction == 0 and not is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, movement_data.air_resistance * delta)
 
 
 func update_animations(direction):
